a5a2f574d8e8804b5adf9047adc0e5bee095e80a
Ikram Hussain <64573967+ikram78@users.noreply.github.com>
2023-10-16 22:13:52 +0530

    Initial commit

---
+++ src/Interface.ts
@@ -0,0 +1,18 @@
+type companyType = {
+  name: string;
+};
+type addressType = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+};
+export interface userData {
+  id: number;
+  userId: string;
+  username: string;
+  name: string;
+  age: number;
+  companyName: string;
+  address: addressType;
+}
